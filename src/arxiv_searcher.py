b7ad79311a65b431c56885c0f9950021c12610a9
Bernardo Denkvitts <bernardoarcari@gmail.com>
2026-08-22 01:29:19 -0400

    reduced delay when retrieving over 300 papers, adjusted log message

--- src/arxiv_searcher.py
+++ src/arxiv_searcher.py
@@ -10,6 +10,8 @@ import arxiv
 from arxivql.taxonomy import categories_by_id
 from arxivql import Query, Taxonomy as T
 
+from streamlit import cache_resource
+
 import pandas as pd
 from exceptions import (
     InvalidCategoryError,
@@ -152,7 +154,7 @@ def search(keywords: str, start_date: datetime.date, end_date: datetime.date, so
 
     client = arxiv.Client(
         page_size=300,
-        delay_seconds=8,
+        delay_seconds=3, # At each 300 papers retrieved, there will be a delay of 3 seconds
         num_retries=4
     )
     query = build_arxiv_query(keywords=keywords, category=category)
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import logging
 from functools import wraps
 
 def timeit(func):
-    """Decorator for measuring the exact time required to perform tasks"""
+    """Decorator to measure the exact execution time of functions."""
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -11,6 +11,6 @@ def timeit(func):
         end_time = time.perf_counter()
         
         total_time = end_time - start_time
-        logging.info(f"The function '{func.__name__}' finished in {total_time:.4f} seconds.")
+        logging.info(f"[TIMER] Finished executing '{func.__name__}'. Total time elapsed: {total_time:.4f} seconds.")
         return result
     return timeit_wrapper
